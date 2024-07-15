77c1093402cc103743e62e5527b8a8d9310d1a73
henrymedeiros <henrymedeiros77@gmail.com>
2024-07-15 04:13:34 -0300

    add better way to quit execution by using threading

--- nodes_no_walk.py
+++ nodes_no_walk.py
@@ -1,33 +1,41 @@
+import threading
 import keyboard
 import pyautogui
 import time
 import argparse
 
 #TODO
-# better way to quit execution
+# optional argument to implement random delay to wait node replenish
+
+def stop_loop():
+    global running
+    running = False
+    print("You pressed 'q', the loop will now stop.")
 
 def map_nodes(num_nodes):
     nodes = []
     for i in range(1, num_nodes+1):
-        print(f'Position the mouse on the {i}º node and press Enter.')
+        print(f'Position the mouse on node {i} and press Enter.')
         keyboard.wait('enter')
         node_position = pyautogui.position()
-        print('Node position: ', node_position)
-        time.sleep(.5)
+        print('Node position:', node_position)
+        time.sleep(0.5)
         nodes.append(node_position)
     return nodes
 
 def run_extraction_loop(nodes, interval):
-    while True:
+    global running
+    running = True
+    while running:
         for node in nodes:
-            if keyboard.is_pressed('q'):
-                print("You pressed 'q', the loop will now stop.")
+            if not running:
                 break
             pyautogui.click(node)
             time.sleep(interval)
-        if keyboard.is_pressed('q'):
-            print("You pressed 'q', the loop will now stop.")
-            break
+
+def monitor_key_press():
+    keyboard.wait('q')
+    stop_loop()
 
 def main():
     parser = argparse.ArgumentParser()
@@ -36,12 +44,14 @@ def main():
     args = parser.parse_args()
 
     nodes = map_nodes(args.n)
-    interval = args.i
-
-    print('Nodes interaction interval: ', interval)
+    print('Nodes interaction interval:', args.i)
     print('Starting extraction loop...')
     time.sleep(2.5)
 
-    run_extraction_loop(nodes, interval)
+    # Starting the keyboard monitoring in a separate thread
+    threading.Thread(target=monitor_key_press, daemon=True).start()
+
+    run_extraction_loop(nodes, args.i)
+
 if __name__ == '__main__':
     main()
